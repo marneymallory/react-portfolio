5272c0a40195875390282cc4094f7ce09a0790ee
marneymallory <79782193+marneymallory@users.noreply.github.com>
2021-09-16 08:32:49 -0800

    add bulk of portfolio project, still some bugs to fix

--- README.md
+++ README.md
@@ -1,7 +1,75 @@
-# portfolio
+# Marney Mallory React Portfolio
 
-Clone/Download the repo.
-Run npm install.
-Change the values in src/resumeData.js to suit your use-case.
-Run npm start to spin the up the local dev server port 3000.(http://localhost:3000).
-Make required changes in src/resumeData.js to suit your needs.
+### 9/16/21 by Marney Mallory
+
+## Overview
+Portfolio showcase of my skills and accomplishments.
+
+## Technologies Used
+
+* 📝 _HTML_
+* 🎨 _CSS_
+* ☕ _JavaScript_
+* 🧭 _Node_
+* 💾 _Webpack_
+* ⚛ _React_
+
+
+## Setup Instructions
+
+1. Clone the repository: `$ git clone https://github.com/marneymallory/react-portfolio`.
+2. Navigate to the `react-portfolio/` directory on your computer.
+3. Open with your preferred text editor to view the code base.
+4. To start a development server and view the project in the browser:
+
+    * _Navigate to `react-portfolio/` in your command line_
+    * _Run the command `npm install` to install dependencies_
+    * _Optionally, run the command `npm run build` to make a bundle of the files_
+    * _Finally, run the command `npm run start` to start a development server_
+
+## Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+## 🐛 Known Bugs
+* _No known bugs_
+
+## License
+_MIT © 2021 Marney Mallory_
+
+## Contact Information
+_Marney Mallory <marney.mallory@gmail.com>_
+
+
--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,98 @@
 let resumeData = {
-    "imagebaseurl":"https://ivy555.github.io/",
-    "name": "Dilip",
-    "role": "Full Satck Developer",
-    "linkedinId":"Your ID",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/dilip-safi-438322a3/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"https://ivy555.github.io/",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"skype Id",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a Masters student at The Hochschule Fulda and pursuing my masters in Global software Development from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://ivy555.github.io",
-    "education":[
-      {
-        "UniversityName":"",
-        "specialization":"Machine Learning",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2022",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://github.com/marneymallory",
+  name: "Marney Mallory",
+  role: "Junior Web Developer",
+  roleDescription:
+    " I love programming because I love solving problems and being in an environment where I can continually learn and grow. I also enjoy creating something out of nothing and building beautiful and creative webpages out of clean and simple code.",
+  socialLinks: [
+    {
+      name: "linkedIn",
+      url: "https://www.linkedin.com/in/marney-mallory/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/marneymallory",
+      className: "fa fa-github",
+    },
+    {},
+  ],
+  aboutme1:
+    "My name is Marney Mallory. I am a junior full-stack developer with a passion for clean code, creativity, problem solving and collaborating with others. I am a recent graduate from Epicodus; a 6 month long full-stack coding bootcamp. Working in this kind of environment has strengthened my ability to communicate concisely and work cooperatively as a team towards a common goal. As an Epicodus student I have experience in JavaScript, HTML, CSS, C#, React, and .Net/.Net Core as well as Node.js, MySQL databases and Test Driven Development with Jest. Epicodus has provided me with a growth mindset and I am confident that I will be able to pick up new concepts quickly and efficiently.",
+  aboutme2:
+    "My job experience prior to programming involved a 5-year career as a dental lab technician fabricating both dentures and crowns. As a technician, I learned how to create quality work while adhering to strict deadlines, communicate effectively inter-departmentally as well as with outside accounts, use my artistic skills to create lifelike fabrications while paying extreme attention to detail, and use my analytic and improvisational skills to complete a “rush case” after a last-minute material malfunction. My years as a dental lab technician taught me the valuable skills of communication, adaptability and ingenuity that I implement daily as a programmer. I decided to make the transition from the dental field to computer programming because I love solving problems and wanted a job where I can continually learn and grow. My time as a developer has been one of the most rewarding and character-building experiences of my life.",
+  email: "marney.mallory@gmail.com",
+  education: [
+    {
+      UniversityName: "Epicodus",
+      Specialization: "Full-Stack Web Developmemnt",
+      StartingMonth: "March ",
+      StartingYear: "2021",
+      MonthOfPassing: "July ",
+      YearOfPassing: "2021",
+      Achievements: "Certificate of Web and Mobile Development",
+    },
+    {
+      UniversityName: "Portland Community College",
+      Specialization: "Dental Lab Technology",
+      StartingMonth: "September ",
+      StartingYear: "2013",
+      MonthOfPassing: "June ",
+      YearOfPassing: "2015",
+      Achievements: "Associates of Science",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "UGrow",
+      Specialization: "Software Engineer Intern",
+      StartingMonth: "August ",
+      StartingYear: "2021",
+      MonthOfLeaving: "September ",
+      YearOfLeaving: "2021",
+    },
+    {
+      CompanyName: "Precision Dental Designs",
+      Specialization: "Crown and Bridge/Denture Department",
+      StartingMonth: "February ",
+      StartingYear: "2016",
+      MonthOfLeaving: "February ",
+      YearOfLeaving: "2021",
+    },
+  ],
+  skillsDescription:
+    "I am creative, have excellent people skills, posess a strong work ethic, pay extreme attention to detail and am very fun to be around!",
+  skills: [
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "React",
+    },
+    {
+      skillname: "C#",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Moonstone Tarot",
+      description:
+        "A Tarot reading application built in React that provides the user with a three card pull, providing insite into their past, present and future",
+      imgurl: "images/portfolio/tarot.jpg",
+      url: "https://tarot-virid.vercel.app/",
+    },
+    {
+      name: "Grounded",
+      description:
+        "A collaboratively built C# application that allows the user to select from a list of emotions and provides calming and grounding excercises based on that emotion.",
+      imgurl: "images/portfolio/grounded.jpg",
+    },
+  ],
+};
+export default resumeData;
